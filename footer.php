207faa523c977991c28db257157df112a5521df4
Lynn Wallenstein <lwallenstein@gmail.com>
2008-11-23 02:02:13 -0500

    RSS Feed Icons

--- footer.php
+++ footer.php
@@ -6,8 +6,11 @@
 				<p>
 					<?php bloginfo('name'); ?> is proudly powered by
 					<a href="http://wordpress.org/">WordPress</a>
-					<br /><a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a>
-					and <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>.
+					<div id="footer_rss_links">
+						<a href="<?php bloginfo('rss2_url'); ?>"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/feed.png" alt="RSS Feed"> Entries</a>
+						and
+						<a href="<?php bloginfo('comments_rss2_url'); ?>"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/feed.png" alt="RSS Feed"> Comments</a>.
+					</div>
 					<!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. -->
 				</p>
 			</div>
--- sidebar.php
+++ sidebar.php
@@ -41,6 +41,8 @@
 			</li> <?php }?>
 
 			<?php wp_list_pages('title_li=<h2>Pages</h2>' ); ?>
+			
+			<?php get_calendar(); ?>  
 
 			<li><h2>Archives</h2>
 				<ul>
@@ -49,6 +51,14 @@
 			</li>
 
 			<?php wp_list_categories('show_count=1&title_li=<h2>Categories</h2>'); ?>
+			
+			
+			<li><h2>Tag Cloud</h2>
+				<ul>
+				<?php wp_tag_cloud(''); ?>
+				</ul>
+			</li>			
+
 
 			<?php /* If this is the frontpage */ if ( is_home() || is_page() ) { ?>
 				<?php wp_list_bookmarks(); ?>
@@ -58,8 +68,6 @@
 					<?php wp_register(); ?>
 					<li><?php wp_loginout(); ?></li>
 					<li><a href="http://validator.w3.org/check/referer" title="This page validates as XHTML 1.0 Transitional">Valid <abbr title="eXtensible HyperText Markup Language">XHTML</abbr></a></li>
-					<li><a href="http://gmpg.org/xfn/"><abbr title="XHTML Friends Network">XFN</abbr></a></li>
-					<li><a href="http://wordpress.org/" title="Powered by WordPress, state-of-the-art semantic personal publishing platform.">WordPress</a></li>
 					<?php wp_meta(); ?>
 				</ul>
 				</li>
